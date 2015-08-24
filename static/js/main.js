d192f84b0006a269e07e710a06719c821b1f90eb
Nick Penaranda <nick@beepdevices.com>
2015-08-24 12:05:38 -0700

    Fix up front-end rendering
    Previous code was still set up to retrieve and render pre-built Chart.js
    data objects.  New code synthesizes aggregates and the Chart.js data
    object

--- static/js/main.js
+++ static/js/main.js
@@ -1,14 +1,16 @@
-var CACHE_SIZE = 10
+var CACHE_SIZE = 25
+var MAX_LABELS = 20
+var MAX_BINS = 100
 
 var chart = null
 var cache = {}
 var cacheMRU = []
 
-function cacheUpdateMRU(filename) {
-    function indexInCache(filename) {
+function cacheUpdateMRU(id) {
+    function indexInCache(id) {
         var i;
         for (i = 0; i < cacheMRU.length; i++) {
-            if (cacheMRU[i] === filename) {
+            if (cacheMRU[i] === id) {
                 return i
             }
         }
@@ -16,13 +18,13 @@ function cacheUpdateMRU(filename) {
         return -1
     }
 
-    var i = indexInCache(filename)
+    var i = indexInCache(id)
     if (i >= 0) {
         // Is in cache
         cacheMRU.splice(i, 1)
     }
 
-    cacheMRU.push(filename)
+    cacheMRU.push(id)
 
     if (cacheMRU.length > CACHE_SIZE) {
         var f = cacheMRU.shift()
@@ -30,21 +32,63 @@ function cacheUpdateMRU(filename) {
     }
 }
 
-function cacheResult(filename, result) {
-    cache[filename] = result
-    cacheUpdateMRU(filename)
+function cacheResult(id, result) {
+    cache[id] = result
+    cacheUpdateMRU(id)
 }
 
-function cacheCheck(filename) {
-    var result = cache[filename]
+function cacheCheck(id) {
+    var result = cache[id]
     if (result != null) {
-        cacheUpdateMRU(filename)
+        cacheUpdateMRU(id)
     }
 
     return result
 }
 
-function getData(ctx, filename) {
+function getData(ctx, id) {
+
+    function buildData(result) {
+        var labels = []
+        var values = []
+
+        // Decimate data by binning into MAX_BINS
+        var rawLen = result["samples"].length
+        var binSize = Math.ceil(rawLen / MAX_BINS)
+        for (var i = 0; i < MAX_BINS; i++) {
+            var sampleSlice = result["samples"].slice(binSize * i, binSize * (i + 1))
+            var avgPower = sampleSlice
+                    .reduce(function(p,c) { return c["power"] + p }, 0)
+                    / sampleSlice.length
+            values[i] = avgPower
+            labels[i] = ((sampleSlice[sampleSlice.length - 1]["freq"] +
+                    sampleSlice[0]["freq"]) / 20e5) + "Mhz"
+        }
+
+        /*
+        var labelModulus = Math.floor(len / MAX_LABELS)
+
+        for (var i = 0; i < len; i++) {
+            var e = result["samples"][i]
+            labels[i] = i % labelModulus == 0 ? e["freq"] / 10e6 + "MHz" : ""
+            values[i] = e["power"]
+        }
+        */
+
+        return {
+            "labels":labels,
+            "datasets":[{
+                  "pointHighlightFill": "#fff",
+                  "fillColor": "rgba(0,0,0,0.2)",
+                  "pointHighlightStroke": "rgba(0,0,0,1.0)",
+                  "pointColor": "rgba(0,0,0,1.0)",
+                  "strokeColor": "rgba(0,0,0,1.0)",
+                  "pointStrokeColor": "#fff",
+                  "data": values
+            }]
+        }
+    }
+
     function popChart(data) {
         if (chart != null) {
             chart.destroy()
@@ -60,20 +104,22 @@ function getData(ctx, filename) {
         })
     }
 
-    var cachedResult = cacheCheck(filename)
+    var cachedResult = cacheCheck(id)
     if (cachedResult) {
         popChart(cachedResult)
         return
     }
 
     $.ajax({
-        url: filename,
+        url: "/sample",
+        data: {"survey_id": id},
         success: function(result, status) {
-            cacheResult(filename, result)
-            popChart(result)
+            var data = buildData(result)
+            cacheResult(id, data)
+            popChart(data)
         },
         error: function(xhr, status, error) {
-            console.log('error => ', xhr, status, error)
+            console.log("error => ", xhr, status, error)
         }
     });
 }
@@ -83,13 +129,15 @@ $(document).ready(function() {
 
     // Populate data selector
     $.ajax({
-        url: '/survey',
+        url: "/survey",
         success: function(result) {
             var selector = $("#dataSelector")
-            for (r in result) {
-                var item = document.createElement('option')
-                item.value = result[r]
-                item.innerHTML = result[r].replace('.txt.json','')
+            var len = result["surveys"].length;
+            for (var i = 0; i < len; i++) {
+                var item = document.createElement("option")
+                var surveyObj = result["surveys"][i]
+                item.value = surveyObj["id"]
+                item.innerHTML = surveyObj["label"] + " @ " + surveyObj["time"]
                 selector.append(item)
             }
         }
